a68f7aae20a89092f8ced775e8f443fc7c68f634
Courtney <u17079782@tuks.co.za>
2020-10-15 08:44:29 +0200

    2020/10/15

--- src/app/components/reports/stage-report/stage-report.component.ts
+++ src/app/components/reports/stage-report/stage-report.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
+import { ToastsService } from 'src/app/services/toasts.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Audit } from 'src/app/models/audit';
 
 @Component({
   selector: 'app-stage-report',
+  host: {class:'full-component'},
   templateUrl: './stage-report.component.html',
   styleUrls: ['./stage-report.component.css']
 })
 export class StageReportComponent implements OnInit {
 
-  constructor() { }
+  audits : Audit[] = [];
+
+  constructor( private modal : NgbModal, private toast : ToastsService, private api : ApiService, private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.getData();
+  }
+
+  getData(){
+
+    this.getAudits();
+  }
+
+  getAudits(){
+    this.api.getAudits().subscribe( success => this.getAuditSuccess(success), error => this.getAuditFail(error));
+  }
+  //success
+  getAuditSuccess(success){
+    this.audits = success;
+  }
+  //fail
+  getAuditFail(error){
+    this.toast.display({type : "Error", heading :error.error.Title, message : error.error.message });
   }
 
 }
+
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -711,7 +711,7 @@ export class ApiService {
     return this.http.post( this.booking, {request : "cancelBooking", payload })
   }
   // edituserBooking(){
-  //   THERE IS NO EDIT SO I DONNO IF I SHOULD EDIT OR A USER NEEDS TO FIRST CANCEL A BOOKING  
+  //   THERE IS NO EDIT SO I DONNO IF I SHOULD EDIT OR A USER NEEDS TO FIRST CANCEL A BOOKING
   // }
   getAssistantSearch(){
     return this.http.post( this.booking, {request : "getAssistantSearch"});
@@ -749,7 +749,6 @@ export class ApiService {
   getAudits(){
     return this.http.post<Audit[]>( this.audit, {request : "getAudits" });
   }
-
 
 
 }
