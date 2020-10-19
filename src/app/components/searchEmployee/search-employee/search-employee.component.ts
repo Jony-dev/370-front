eaed2c85afbca518ef983606a66f3a42ce570d6c
unknown <u16278055@tuks.co.za>
2020-10-19 23:30:40 +0200

    Co-authored-by: marli@dubuson.com <marli@dubuson.com> Co-authored-by: u17155500@up.ac.za <u17155500@up.ac.za>

--- src/app/components/searchEmployee/search-employee/search-employee.component.ts
+++ src/app/components/searchEmployee/search-employee/search-employee.component.ts
@@ -3,6 +3,9 @@ import { ApiService } from 'src/app/services/api.service';
 import { ToastsService } from 'src/app/services/toasts.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SearchUser} from 'src/app/models/searchUser';
+import { EditProfileMComponent } from '../../modals/editProfile/edit-profile-m/edit-profile-m.component';
+import { userCard } from 'src/app/models/userCard';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-search-employee',
@@ -15,6 +18,9 @@ export class SearchEmployeeComponent implements OnInit {
   constructor( private modal : NgbModal , private api : ApiService, private toast : ToastsService) { }
 
   users : SearchUser[] = [];
+  userProfileCard : userCard;
+  user : User;
+
 
   ngOnInit(): void {
 
@@ -30,6 +36,7 @@ export class SearchEmployeeComponent implements OnInit {
   getSearchUser(){
 
     this.api.getUsersForSearch().subscribe( s => this.getSearchSuccess(s), e => this.getSearchError(e));
+    console.log(this.users);
 
   }
   getSearchSuccess(s){
@@ -38,6 +45,25 @@ export class SearchEmployeeComponent implements OnInit {
   }
   getSearchError(e){
     this.toast.display({type : 'Error', heading : e.error.Title, message : e.error.message });
+  }
+
+  editProfile(){
+   /* const editProfileInstance = this.modal.open(EditProfileMComponent,  { windowClass : "largeModal",backdrop:"static" });
+    editProfileInstance.result.then((res) =>{
+
+      console.log(res);
+      if(res)
+      {
+
+        this.user.imgUrl = res.imgUrl;
+        this.user.name = res.name;
+        this.user.surname = res.surname;
+
+
+      }
+
+    })*/
+
   }
 
 
